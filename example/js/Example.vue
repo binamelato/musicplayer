2645b4f092d7e66a3800a3450605757354eaa372
Ilos <shahral@mail.ru>
2022-08-21 14:19:28 +0300

    add content

---
+++ example/js/Example.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+        <app-input :msg="message" @messChange="message = $event"></app-input>
+    </div>
+</template>
+
+<script>
+    import Input from './field.vue'
+
+    export default {
+        data(){
+            return {
+                message: 'Test work text'
+            }
+        },
+       components: {
+           'app-input': Input
+       }
+    }
+</script>
